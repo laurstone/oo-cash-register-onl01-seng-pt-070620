1eb3d02c9cc1483ef88a1541e0bdfe1e7fbda27b
Lauren Stone <lstone321@gmail.com>
2020-08-03 22:48:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     quantity.times {@items << item}
     @total += price * quantity 
-    @last_transaction = price
+    @last_transaction = price * quantity
   end
 
   def apply_discount
